9d670aa6c9ecbad06cb96e9b30c18ab53b5c6759
NathaDCL <nathac1203@gmail.com>
2025-10-16 21:12:15 -0300

    A

---
+++ ApiArchivo/Controllers/AController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+namespace ApiArchivo.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class AController : ControllerBase
+    {
+        [HttpDelete("EliminarArchivo/{archivo}")]
+        public string Delete(string archivo)
+        {
+         String retorno = "";
+            if (!Directory.Exists("C:\\Api"))
+            {
+               
+                retorno = "Directorio inexistente";
+
+            }
+            else
+            {
+
+            
+            if (!System.IO.File.Exists($"C:\\Api\\{archivo}.txt"))
+            {
+                
+                retorno = "archivo inexistente";
+
+            }
+            else
+            {
+                    if (System.IO.File.Exists($"C:\\Api\\{archivo}.txt"))
+                        {
+                        System.IO.File.Delete($"C:\\Api\\{archivo}.txt");
+                    }
+            }
+            
+            }
+            return retorno;
+        }
+            
+        }
+    }
---
+++ ApiArchivo/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+
+namespace ApiArchivo.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpPost("CrearArchivo/{archivo}")]
+        public string Post(string archivo)
+        {
+            String retorno = "";
+            if (!Directory.Exists("C:\\Api"))
+            {
+                Directory.CreateDirectory("C:\\Api");
+                retorno = "Directorio creado y";
+
+            }
+            
+            if (!System.IO.File.Exists($"C:\\Api\\{archivo}.txt"))
+            {
+                System.IO.File.Create($"C:\\Api\\{archivo}.txt");
+                retorno = retorno + " archivo creado";
+
+            }
+            else
+            {
+                retorno = "archivo ya existente";
+            }
+            return retorno;
+            
+        }
+    }
+}
